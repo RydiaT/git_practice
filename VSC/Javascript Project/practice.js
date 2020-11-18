d2955b77df6193e8c8fb44f4247239512490ef40
AAAAHHHHHH <rydia.a.titus@gmail.com>
2020-11-17 19:20:57 -0600

    Index Added

--- VSC/Javascript Project/practice.js
+++ VSC/Javascript Project/practice.js
@@ -6,3 +6,4 @@ let messageToSend = message => {
         return 'Uuuhh... sorry, but thats gotta be a string. Try adding quotes or making sure your quotes are the same types.'
     }
 }
+//The name for this project is missleading- I'm using it for practicing making websites.
