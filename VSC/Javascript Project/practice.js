a78607bdf7aee5918f2d32b1588ba0a661ac2c9b
AAAAHHHHHH <rydia.a.titus@gmail.com>
2020-11-18 15:11:06 -0600

    Changed the comment

--- VSC/Javascript Project/practice.js
+++ VSC/Javascript Project/practice.js
@@ -6,4 +6,5 @@ let messageToSend = message => {
         return 'Uuuhh... sorry, but thats gotta be a string. Try adding quotes or making sure your quotes are the same types.'
     }
 }
-//The name for this project is missleading- I'm using it for practicing making websites.
+//The name for this project is missleading- I'm using it for practicing making websites. Currently, my plan is for this to be a game where you have either rock,
+//paper, or scissors, and train them to fight other things.
